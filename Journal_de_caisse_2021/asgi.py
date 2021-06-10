974ecb004d5e2220eb83e9c512f87f7080266744
Jonckers <etiennejonckers@gmail.com>
2021-06-10 13:35:18 +0200

    Initial commit

---
+++ Journal_de_caisse_2021/asgi.py
@@ -0,0 +1,45 @@
+"""
+ASGI config for Journal_de_caisse_2021 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Journal_de_caisse_2021.settings')
+
+jdc_application = get_asgi_application()
+
+async def application(scope, receive,send):
+    if scope['type'] == 'http':
+        #let django handle http request
+        await jdc_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        #we'll handle websocket request here
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type '{scope['type']}'")
+
+
+async def websocket_application(scope, receive, send):
+    while True:
+        event = await receive()
+
+        if event['type'] == 'websocket.connect':
+            await send({
+                'type': 'websocket.accept',
+            })
+        if event['type'] == 'websocket.disconnect':
+            break
+
+        if event['type'] == 'websocket.receive':
+            if event['text'] == 'ping':
+                await send({
+                    'type': 'websocket.send',
+                    'text': 'png!'
+                })
---
+++ JournauxDeCaissePbrussels/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JournauxdecaissepbrusselsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'JournauxDeCaissePbrussels'
