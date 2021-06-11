82a0cde25ccc24d6a25536e9eed49b1f0a690907
Jonckers <etiennejonckers@gmail.com>
2021-06-11 15:04:47 +0200

    Add 4 class Models

--- Journal_de_caisse_2021/settings.py
+++ Journal_de_caisse_2021/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'JournauxDeCaissePbrussels.apps.JournauxdecaissepbrusselsConfig',
+    'JournauxDeCaissePBrussels_Web',
 ]
 
 MIDDLEWARE = [
--- JournauxDeCaissePbrussels/models.py
+++ JournauxDeCaissePbrussels/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    function = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    date = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    amonds = models.DecimalField(max_digits=10,decimal_places=2)
+    communications = models.TextField()
+
+class Payment(models.Model):
+    date = models.DateTimeField()
+    name = models.CharField(max_length=100)
+    amonds = models.DecimalField(max_digits=10, decimal_places=2)
+    plate = models.charField(max_length=8)
+
+class Amonds(models.Model):
+    recipeFK = models.ForeignKey('Recipe', on_delete=models.CASCADE,)
+    payementFK = models.ForeignKey('Payment', on_delete=models.CASCADE,)
+    employeeFK = models.ForeignKey('Employee', on_delete=models.CASCADE,)
